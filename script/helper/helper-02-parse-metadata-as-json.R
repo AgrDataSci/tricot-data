d7a82f24a4fe780838900feb362c5e3714767fb5
kauedesousa <kaue.desousa@inn.no>
2025-09-13 12:25:26 +0200

    zenodo json

--- script/helper/helper-02-parse-metadata-as-json.R
+++ script/helper/helper-02-parse-metadata-as-json.R
@@ -10,7 +10,8 @@ metadata = read_excel(file, sheet = "metadata")
 authors = read_excel(file, sheet = "authors")
 funders = read_excel(file, sheet = "funders")
 communities = read_excel(file, sheet = "communities")
-dates = read_excel(file, sheet = "dates")
+dates = NULL
+
 
 keep = grep("1", authors$checked_dataset)
 authors = authors[keep, ]
@@ -32,17 +33,6 @@ if ("orcid" %in% names(authors)) {
     print(orcid_invalid[, c("given_name", "family_name", "orcid")])
   } else {
     cat("All ORCIDs are valid (or empty).\n")
-  }
-}
-
-# Check date format
-if (!is.null(dates)) {
-  bad_dates = dates[!grepl("^\\d{4}-\\d{2}-\\d{2}$", as.character(dates$date)), ]
-  if (nrow(bad_dates) > 0) {
-    cat("Invalid date format in 'dates' sheet (expected YYYY-MM-DD):\n")
-    print(bad_dates)
-  } else {
-    cat("All dates are in valid format.\n")
   }
 }
 
@@ -133,3 +123,11 @@ if (!is.null(funders)) {
 
 # Write to JSON
 write(toJSON(datacite, pretty = TRUE, auto_unbox = TRUE), ".zenodo.json")
+
+# library("jsonvalidate")
+# 
+# schema = "datacite_4.5_schema.json"
+# json = ".zenodo.json"
+# 
+# v = json_validator(schema, engine = "ajv")
+# v(json, verbose = TRUE)
